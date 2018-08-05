3640acac1fff480c0fe506942510c9041d5af9e2
Alexander <sanchezzz90210@yandex.ru>
2018-08-05 22:31:58 +0300

    kv file modified.

--- memorizer.py
+++ memorizer.py
@@ -2,32 +2,35 @@
 # memorizer main file
 
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.clock import Clock
+# from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+# from kivy.uix.label import Label
+# from kivy.clock import Clock
+# from kivy.properties import ObjectProperty
 
 
 import time
+import random
 
-class Memorizer(Widget):
 
-    def update(self, delta=30):
-        start = time.time()
-        elapsed_time = time.time -  start
-        # while elapsed_time <= delta:
-        while True:
-            if elapsed_time <= delta:
-                time_left = delta - elapsed_time
-                time.sleep(1)
-                # function showing time left to be called here
-            else:
-                break
+TIME = 10
+
+class Memorizer(BoxLayout):
+
+    def time_left(self, time=TIME):
+        while time:
+            time -= 1
+            # time.sleep(1.0)
         return None
+
+    def number(self):
+        return random.randint(0, 999999)
 
 class MemorizerApp(App):
     def build(self):
         self.title = 'Memorizer'
         memorizer = Memorizer()
-        Clock.schedule_interval(memorizer.update, 1.0)
+        # Clock.schedule_interval(memorizer.time_left, 1.0)
         return memorizer
 
 
