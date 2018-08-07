8692f5ad41b314d49a1acfce74851508f411c645
Alexander <sanchezzz90210@yandex.ru>
2018-08-08 01:44:38 +0300

    Memorize screen

--- memorizer.py
+++ memorizer.py
@@ -5,7 +5,7 @@ from kivy.app import App
 # from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 # from kivy.uix.label import Label
-# from kivy.clock import Clock
+from kivy.clock import Clock
 # from kivy.properties import ObjectProperty
 
 
@@ -13,24 +13,34 @@ import time
 import random
 
 
-TIME = 10
+# count = 10
+count = 3
 
 class Memorizer(BoxLayout):
 
-    def time_left(self, time=TIME):
-        while time:
-            time -= 1
-            # time.sleep(1.0)
+    def time_left(self, *kwargs): # ????
+        print('Args: {}'.format(kwargs))
+        global count
+        print('count: {}'.format(count))
+        time = str(count) + ' secs left'
+        if count:
+            count -= 1
+            print('count decreased!')
+        else:
+            print("i'm here!")
+            self.ids.time_left.text = 'Time is over!'
+        self.ids.time_left.text = time
         return None
 
     def number(self):
-        return random.randint(0, 999999)
+        return random.randint(100000, 999999)
 
 class MemorizerApp(App):
+
     def build(self):
         self.title = 'Memorizer'
         memorizer = Memorizer()
-        # Clock.schedule_interval(memorizer.time_left, 1.0)
+        Clock.schedule_interval(memorizer.time_left, 1.0)
         return memorizer
 
 
