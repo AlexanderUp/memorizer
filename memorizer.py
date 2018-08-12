ad1fe4a2e5044fe46ae9c4a9abd35e9d8587a30a
Alexander <sanchezzz90210@yandex.ru>
2018-08-12 23:54:48 +0300

    First attempt

--- memorizer.py
+++ memorizer.py
@@ -2,46 +2,76 @@
 # memorizer main file
 
 from kivy.app import App
-# from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
-# from kivy.uix.label import Label
 from kivy.clock import Clock
-# from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager import Screen
+from kivy.properties import BooleanProperty
+from kivy.properties import NumericProperty
 
 
-import time
 import random
 
 
 # count = 10
 count = 3
+# scr = NumericProperty(0)
 
-class Memorizer(BoxLayout):
+class MemoScreen(Screen):
 
-    def time_left(self, *kwargs): # ????
-        print('Args: {}'.format(kwargs))
+    # scr = BooleanProperty(False)
+    scr = NumericProperty(0)
+
+    def time_left(self, *kwargs):
+        # nonlocal scr
+        # global scr
         global count
-        print('count: {}'.format(count))
-        time = str(count) + ' secs left'
+        time_ = str(count) + ' secs left'
         if count:
             count -= 1
             print('count decreased!')
         else:
-            print("i'm here!")
-            self.ids.time_left.text = 'Time is over!'
-        self.ids.time_left.text = time
-        return None
+            time_ = 'Time is over!'
+            # Clock.unschedule(self.time_left)
+            print('Unscheduled!')
+            # sm.current = 'Check'
+            # print('BooleanProperty before: {}'.format(scr))
+            # scr = True
+            # repr(scr)
+            print('NumericProperty before: {}'.format(self.scr))
+            self.scr = 1
+            print('BooleanProperty: {}'.format(self.scr))
+        self.ids.time_left.text = time_
+        return None # ????
 
     def number(self):
         return random.randint(100000, 999999)
+
+    def on_scr(self, *kwargs):
+        print('on_scr called!')
+        print('*kwargs: {}'.format(kwargs))
+        # return sm.current('Check')
+        # check = CheckScreen()
+        # return check
+
+
+class CheckScreen(Screen):
+    pass
+
 
 class MemorizerApp(App):
 
     def build(self):
         self.title = 'Memorizer'
-        memorizer = Memorizer()
-        Clock.schedule_interval(memorizer.time_left, 1.0)
-        return memorizer
+        # memorizer = MemoScreen()
+        # Clock.schedule_interval(memorizer.time_left, 1.0)
+        # return memorizer
+        sm = ScreenManager()
+        sm.add_widget(MemoScreen(name='MemoScr'))
+        sm.add_widget(CheckScreen(name='CheckScr'))
+        # Clock.schedule_interval(memorizer.time_left, 1.0)
+        return sm
+
 
 
 if __name__ == '__main__':
